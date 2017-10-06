a739aed74e9a3fc6ba50d0a7dc498f6acdda10b4
Heather Arnold <harnold4882@gmail.com>
2017-10-06 11:19:36 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,4 +3,5 @@
 puts colors "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
 end
 
+print display_rainbow(colors)
 #display_rainbow method here
